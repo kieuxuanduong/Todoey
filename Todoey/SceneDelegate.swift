1fdc81cb23757772c221122a26a6ab477a3a8d6d
Kieu Xuan Duong <kieuxuanduong>
2020-09-06 23:53:17 +0700

    Persisting data using Core Data

--- Todoey/SceneDelegate.swift
+++ Todoey/SceneDelegate.swift
@@ -48,6 +48,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
         print("applicationDidEnterBackground")
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
